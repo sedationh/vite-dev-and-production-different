631aad52921eccd4d5ada79ba55be9cfc6c0e8a9
SedationH <sedationh@gmail.com>
2023-11-17 16:30:12 +0800

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import * as x from "build"
+
+console.log("Mode", import.meta.env.MODE)
+console.log("x", x)
+
 function App() {
   return <>This is App</>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import * as x from "build"
-
-console.log("sedationh x", x)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
